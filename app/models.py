67a610ddf668ee282bd3438be1eb4eb595eb2873
Oded Masala <odedmasala6@gmail.com>
2025-06-20 23:15:51 +0300

    feat: fix main.py

--- app/models.py
+++ app/models.py
@@ -1,8 +1,9 @@
 # File: app/models.py
 from sqlalchemy import Column, Integer, Text
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
+
 
 class QARecord(Base):
     __tablename__ = "qa_records"
