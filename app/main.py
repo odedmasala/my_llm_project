45d3636d62c48d48f3ef2195d9689e0ce48852d5
Oded Masala <odedmasala6@gmail.com>
2025-06-20 23:15:51 +0300

    feat: fix main.py

--- app/main.py
+++ app/main.py
@@ -1,35 +1,36 @@
 # File: app/main.py
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
-from transformers import pipeline
+from fastapi.staticfiles import StaticFiles
+from transformers.pipelines import pipeline
 
+from app.db import AsyncSessionLocal, engine
 from app.models import Base, QARecord
-from app.db import engine, AsyncSessionLocal
 
 app = FastAPI()
 qa = pipeline("question-answering", model="distilbert-base-uncased-distilled-squad")
 
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
+
 @app.on_event("startup")
 async def startup_event():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
+
 @app.get("/")
 def serve_home():
     return FileResponse("app/static/index.html")
 
+
 @app.get("/ask")
 async def ask(question: str, context: str):
-    result = qa(question=question, context=context)
+    result = qa(question=question, context=context)  # type: ignore[call-arg]
 
     async with AsyncSessionLocal() as session:
         qa_record = QARecord(
-            question=question,
-            context=context,
-            answer=result["answer"]
+            question=question, context=context, answer=result["answer"]
         )
         session.add(qa_record)
         await session.commit()
